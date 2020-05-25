cee787e2f7ba09d546cd4aa9698b8ab9eab76822
Joshua Kent <hyperxbeat@live.com>
2020-05-25 21:15:27 +0100

    started scientific calculator development

--- included/SciCalc/__init__.py
+++ included/SciCalc/__init__.py
@@ -1,5 +1,5 @@
 from .module import *
 __title__ = "Calculator"
-__version__ = "1.0 22-05-2020"
+__version__ = "1.0.0 25-05-2020"
 __author__ = "Joshua Kent"
 default_args = {"root": "root"}
--- included/SciCalc/module.py
+++ included/SciCalc/module.py
@@ -2,10 +2,20 @@ from tkinter import *
 from tkinter.ttk import *
 
 def init(root):
-    print(root)
+    root.title("Scientific Calculator")
+    root.geometry("400x600+20+20")
+    root.resizable(False, False)
     style = Style()
     style.configure("TFrame", theme = "winnative")
     style.configure("TButton", theme = "winnative")
     frame = Frame(root)
-    btn = Button(frame, text = "yes", style = "TButton").grid()
+    
+    for i in range(10):
+        btn = Button(frame, style = "TButton", text = (i + 1))
+        btn.grid()
     frame.grid()
+
+if __name__ == "__main__":
+    root = Tk()
+    init(root)
+    root.mainloop()
