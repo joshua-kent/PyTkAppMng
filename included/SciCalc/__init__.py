3618e76f6280ba11794445c972ebcfccb7f8497e
Joshua Kent <hyperxbeat@live.com>
2020-05-25 22:53:42 +0100

    added icons

--- included/SciCalc/__init__.py
+++ included/SciCalc/__init__.py
@@ -1,5 +1,5 @@
 from .module import *
 __title__ = "Calculator"
-__version__ = "1.0.2 25-05-2020 22:23 GMT"
+__version__ = "1.0.2 25-05-2020 22:53 GMT"
 __author__ = "Joshua Kent"
 default_args = {"root": "root"}
