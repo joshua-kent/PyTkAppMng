d259f84b6d118ec679ae531dacacbc0eef1de5f8
Joshua Kent <hyperxbeat@live.com>
2020-05-26 15:22:15 +0100

    Moved SciCalc definitions to defs.py

---
+++ included/SciCalc/defs.py
@@ -0,0 +1,14 @@
+buttons_dict = {1: [r"$|a|$", "abs"], 2: [r"$\sqrt{a}$", "sqrt"], 3: [r"$\log$", "log"],
+                    4: [r"$\ln$", "ln"], 5: [r"$a^b$", "power"], 6: [r"$()$", "brackets"],
+                    7: [r"%", "percent"], 8: [r"$=$", "equals"], 9: [r"$\lfloor{a}\rfloor$", "floor"],
+                    10: [r"$f(x)$", "func"], 11: [r"$\cot$", "cot"], 12: [r"$\tan$", "tan"],
+                    13: [r"$7$", "7"], 14: [r"$8$", "8"], 15: [r"$9$", "9"], 16: [r"$\div$", "div"],
+                    17: [r"$\lceil{a}\rceil$", "ceil"], 18: [r"$\frac{d}{dx}$", "derivative"],
+                    19: [r"$\sec$", "sec"], 20: [r"$\cos$", "cos"], 21: [r"$4$", "4"], 22: [r"$5$", "5"],
+                    23: [r"$6$", "6"], 24: [r"$\times$", "times"], 25: [r"$x$", "x"],
+                    26: [r"$\int$", "integral"], 27: [r"$\csc$", "csc"], 28: [r"$\sin$", "sin"],
+                    29: [r"$1$", "1"], 30: [r"$2$", "2"], 31: [r"$3$", "3"], 32: [r"$-$", "minus"],
+                    33: [r"$y$", "y"], 34: [r"$\int^a_b$", "def_integral"], 35: [r"$e$", "e"],
+                    36: [r"$\pi$", "pi"], 37: [r"$\frac{a}{b}$", "fraction"], 38: [r"$0$", "0"],
+                    39: [r"$.$", "decimal_point"], 40: [r"$+$", "plus"]}
+                    # these are the buttons in LaTeX maths mode format
