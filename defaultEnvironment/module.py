91c34245b5dd0b8770d88690cfe180ec71d5a868
Joshua Kent <hyperxbeat@live.com>
2020-05-29 15:59:05 +0100

    added comments and docstrings

--- defaultEnvironment/module.py
+++ defaultEnvironment/module.py
@@ -21,7 +21,7 @@ from tkinter.ttk import *
 from PIL import Image, ImageTk
 from functools import partial
 
-__version__ = "prerelease2.6b3 2020-05-29 02:32 BST"
+__version__ = "prerelease2.6b4 2020-05-29 12:45 BST"
 
 class init:
     current_dir = os.path.dirname(os.path.realpath(__file__))
@@ -41,80 +41,61 @@ class init:
     amount_of_added_apps = 0
 
     def __init__(self, root):
-
+        
+        # gets the directories of modules in 'included' and 'added'
         for files in os.walk(self.included):
-            _root = files[0] # this will always be a directory, see os.walk()
-            if os.path.dirname(_root) == self.included:
-                split_list = _root.split("\\")
+            root_ = files[0] # this will always be a directory, see os.walk()
+            if os.path.dirname(root_) == self.included:
+                split_list = root_.split("\\")
                 if "__pycache__" not in split_list:
-                    self.included_apps_dirs.append(_root)
+                    self.included_apps_dirs.append(root_)
         self.amount_of_included_apps = len(self.included_apps_dirs)
-
         for files in os.walk(self.added):
-            _root = files[0]
-            if os.path.dirname(_root) == self.added:
-                split_list = _root.split("\\")
+            root_ = files[0]
+            if os.path.dirname(root_) == self.added:
+                split_list = root_.split("\\")
                 if "__pycache__" not in split_list:
-                    self.added_apps_dirs.append(_root)
+                    self.added_apps_dirs.append(root_)
         self.amount_of_added_apps = len(self.added_apps_dirs)
 
+        # gets info about each module from their info.json file
         for i in range(self.amount_of_included_apps + self.amount_of_added_apps):
-            if i < self.amount_of_included_apps:
-                split_dir = self.included_apps_dirs[i].split("\\")
-                module = split_dir[len(split_dir) - 1]
-                info_json_directory = os.path.join(self.included_apps_dirs[i], "info.json")
-            else:
-                split_dir = self.added_apps_dirs[i - self.amount_of_included_apps].split("\\")
-                module = split_dir[len(split_dir) - 1]
-                info_json_directory = os.path.join(self.added_apps_dirs[i - self.amount_of_included_apps])
-            
-            try:
-                with open(info_json_directory, "r") as f:
-                    info = json.load(f)
-            except:
-                info = {}
-            try:
-                title = info["title"]
-            except:
-                title = None
-            try:
-                version = info["version"]
-            except:
-                version = None
-            try:
-                author = info["author"]
-            except:
-                author = None
-            try:
-                default_args = info["default-args"]
-            except:
-                default_args = None
-            try:
-                hidden = info["hidden"]
-            except:
-                hidden = False
-            try:
-                icon_antialiasing = info["icon-antialiasing"]
-            except:
-                icon_antialiasing = True
-            
-            if os.path.isfile(os.path.join(self.included, module, "icon.png")):
-                icon = os.path.join(self.included, module, "icon.png")
-            elif os.path.isfile(os.path.join(self.added, module, "icon.png")):
-                icon = os.path.join(self.added, module, "icon.png")
-            else:
-                icon = None
-            
-            info_dictionary = {"title": title, "version": version,
-                "author": author, "directory": self.included_apps_dirs[i],
-                "icon": icon, 'default-args': default_args, "hidden": hidden,
-                "icon-antialiasing": icon_antialiasing}
-            
-            if i < self.amount_of_included_apps:
-                self.included_apps_info[module] = info_dictionary
-            else:
-                self.added_apps_info[module] = info_dictionary
 
+            # sets details based on if it is getting an app from 'included' or 'added'
+            if i < self.amount_of_included_apps:
+                mod_dir = self.included_apps_dirs[i]
+            else:
+                mod_dir = self.added_apps_dirs[i - self.amount_of_included_apps]
+            
+            split_dir = mod_dir.split("\\")
+            module = split_dir[len(split_dir) - 1]
+            info_json_directory = os.path.join(mod_dir, "info.json")
+
+            # gets settings from the info.json file (None if doesn't exist), puts them in info
+            info = {}
+            info["title"] = self.get_setting(info_json_directory, "title", "Unnamed app")
+            info["version"] = self.get_setting(info_json_directory, "version")
+            info["author"] = self.get_setting(info_json_directory, "author", "Unknown")
+            info["directory"] = mod_dir
+            info["default-args"] = self.get_setting(info_json_directory, "default-args")
+            info["hidden"] = self.get_setting(info_json_directory, "hidden", "False")
+            info["icon-antialiasing"] = self.get_setting(info_json_directory, "icon-antialiasing", "True")
+
+            # gets icon from the icon.png file (None if doesnt exist)
+            if os.path.isfile(os.path.join(self.included, module, "icon.png")):
+                info["icon"] = os.path.join(self.included, module, "icon.png")
+            elif os.path.isfile(os.path.join(self.added, module, "icon.png")):
+                info["icon"] = os.path.join(self.added, module, "icon.png")
+            else:
+                info["icon"] = None
+            
+            # sets the respective apps_info dictionary key (module) to (info)
+            if i < self.amount_of_included_apps:
+                self.included_apps_info[module] = info
+            else:
+                self.added_apps_info[module] = info
+
+        # set up window
         self.root = root
         self.root.title("PyTk Application Manager {}".format(__version__))
         self.root.iconphoto(False, ImageTk.PhotoImage(file = self.env_icon))
@@ -124,20 +105,23 @@ class init:
         Grid.rowconfigure(self.root, 0, weight = 0)
         Grid.columnconfigure(self.root, 0, weight = 0)
 
+        # if things already exist in the window (i.e. after another app was previously open), delete them
         for item in self.root.winfo_children():
             item.destroy()
-        
-        self.menu = Menu(self.root)
-        self.root.config(menu = self.menu)
 
         # ttk style
         self.style = Style()
         self.style.configure("frame.TFrame", theme = "winnative")
         self.style.configure("defEnv.TButton", theme = "winnative", relief = "flat")
 
-        self.setup_defaults(self.root, self.style)
+        # setup window attributes in accordance to user_settings.json
+        self.setup_defaults()
 
-        # create file menu
+        # creates menu
+        self.menu = Menu(self.root)
+        self.root.config(menu = self.menu)
+
+        # creates file menu
         file = Menu(self.menu, tearoff = 0)
         file.add_command(label = "Import")
         file.add_command(label = "Return to selection screen", command = partial(init, root))
@@ -147,14 +131,14 @@ class init:
 
         # creates settings menu
         settings = Menu(self.menu, tearoff = 0)
-        settings.add_command(label = "Change background colour", command = lambda: self.recolour_background(root, self.style))
+        settings.add_command(label = "Change background colour", command = self.recolour_background)
         settings.add_command(label = "Save current colour as default",
                                 command = lambda: self.set_setting(self.user_settings, "background", self.root.cget("background")))
         settings.insert_separator(2)
-        settings.add_command(label = "Reset all to default", command = lambda: self.reset_all_defaults(self.root, self.style))
+        settings.add_command(label = "Reset all to default", command = self.reset_all_defaults)
         self.menu.add_cascade(label = "Settings", menu = settings)
 
-        # create frame
+        # creates frame
         self.frame = Frame(self.root, style = "frame.TFrame")
         self.frame.grid_forget()
 
@@ -170,6 +154,8 @@ class init:
         x = 0
         y = 0
         for key, value in self.included_apps_info.items():
+            
+            # all of these values are set to make functionality for additional info in future
             title = value["title"]
             version = value["version"]
             author = value["author"]
@@ -178,6 +164,8 @@ class init:
             default_args = value["default-args"]
             icon_antialiasing = value["icon-antialiasing"]
             hidden = value["hidden"]
+
+            # creates icon (but not if it's hidden)
             if not hidden:
                 if icon != None:
                     icon = Image.open(icon)
@@ -207,6 +195,9 @@ class init:
                     y += 2
         
     def run_module(self, module, root, frame):
+        # runs a module in /applications
+
+        # clear current window (other than its menu)
         for i in self.root.winfo_children():
             if i.winfo_children():
                 if repr(i) != "<tkinter.Menu object .!menu>":
@@ -214,13 +205,20 @@ class init:
                         k.destroy()
         self.root.deiconify()
 
+        # set location to whichever folder it's in
+        # (this could return an error, but shouldn't as this method is only used internally)
         if module in self.included_apps_info.keys():
             location = "applications.included"
         elif module in self.added_apps_info.keys():
             location = "applications.added"
         
+        ## this needs to be changed to a more direct method ('command' in info.json) -- [[
+
+        # imports the module if not already imported
         if module not in sys.modules:
             exec("import {0}.{1} as {1}".format(location, module))
+
+        # sets default_args
         try:
             default_args = self.included_apps_info[module]["default-args"]
         except:
@@ -241,57 +239,73 @@ class init:
             i += 1
         
         try:
-            exec("{}.init({})".format(module, args_string)) # need to update to obj-oriented
+            exec("{}.init({})".format(module, args_string))
         except:
             if "init" not in dir(module):
                 raise Exception("\'{}.init()\' does not exist".format(module))
             else:
                 raise Exception("Could not run \'{}.init({})\'."
                 "Its default arguments may be incorrect".format(module, args_string))
+        
+        ## ]]
 
-    def set_setting(self, file, setting, new):
-        """
-        """
+    @staticmethod
+    def set_setting(file, setting, value = None):
+        # sets a setting in a json file to a value
+        # (file and setting created if does not exist)
+
         with open(file, "w+") as f:
             try:
                 contents = json.load(f)
-            except json.decoder.JSONDecodeError:
+            except:
                 contents = {}
-            contents[setting] = new
+            contents[setting] = value
             f.seek(0)
             json.dump(contents, f, indent = 4)
             f.truncate()
 
-    def get_setting(self, file, setting):
+    @staticmethod
+    def get_setting(file, setting, not_exist = None):
+        # gets the value of a setting in a json file 
+        # (if the file or setting does not exists, returns not_exist)
+
         with open(file, "r+") as f:
             try:
                 contents = json.load(f)
-            except json.decoder.JSONDecodeError:
-                return None
+            except:
+                return not_exist
+            
             if setting in contents:
                 return contents[setting]
             else:
-                return None
+                return not_exist
 
-    def setup_defaults(self, root, style):
+    # self.__class__ acts like cls
+    def setup_defaults(self):
+        # sets up 'root' (Tk obj) and 'style' (Tkk style obj) to match user_defaults.json
+
         if self.get_setting(self.user_settings, "background") == None:
             self.set_setting(self.user_settings, "background", "SystemButtonFace")
-        root.config(background = self.get_setting(self.user_settings, "background"))
-        style.configure("TButton", background = self.get_setting(self.user_settings, "background"))
-        style.configure("TLabel", background = self.get_setting(self.user_settings, "background"))
-        style.configure("TFrame", background = self.get_setting(self.user_settings, "background"))
+        self.root.config(background = self.get_setting(self.__class__.user_settings, "background"))
+        self.style.configure("TButton", background = self.__class__.get_setting(self.__class__.user_settings, "background"))
+        self.style.configure("TLabel", background = self.__class__.get_setting(self.__class__.user_settings, "background"))
+        self.style.configure("TFrame", background = self.__class__.get_setting(self.__class__.user_settings, "background"))
 
-    def reset_all_defaults(self, root, style):
-        with open(self.user_settings, "w+") as f:
+    def reset_all_defaults(self):
+        # resets the user_settings.json folder to an empty file
+
+        with open(self.__class__.user_settings, "w+") as f:
             f.seek(0)
             f.truncate()
             json.dump({}, f)
-        self.setup_defaults(root, style)
-    
-    def recolour_background(self, root, style):
+        self.setup_defaults()
+
+    def recolour_background(self):
+        # recolours the background
+
         colour = colorchooser.askcolor(title = "Choose background colour",
         initialcolor = "SystemButtonFace")
-        root.config(background = colour[1])
-        style.configure("TFrame", background = colour[1])
-        style.configure("TButton", background = colour[1])
-        style.configure("TLabel", background = colour[1])
+        self.root.config(background = colour[1])
+        self.style.configure("TFrame", background = colour[1])
+        self.style.configure("TButton", background = colour[1])
+        self.style.configure("TLabel", background = colour[1])
--- packages/typemathtext/main_class.py
+++ packages/typemathtext/main_class.py
@@ -5,8 +5,10 @@ class typemathtextError(Exception):
     pass
 
 class text:
+    """Creates an object that can be used for easy-to-use methods to create calculators."""
+
     latex_text = ""
-    parsed_latex_text = []
+    parsed_latex_text = [] # LaTeX text separated into a list
     pointer = 0
     current_dir = os.path.dirname(os.path.realpath(__file__))
     parse_info_dir = os.path.join(current_dir, "parse_info.json")
@@ -14,14 +16,63 @@ class text:
     def __init__(self):
         self.parse_latex(r"\int4x+\int2dx-3dx+\int4+\int2dxdx")
 
-    def edit(self, text, moveby = 1, moveto = None):
+    def edit(self, text, insert = None, moveby = 1, moveto = None):
+        """Edits latex text by parsing, editing, repositioning the pointer, recompiling
+
+
+        KEYWORDS
+
+            text (str) -- a string of LaTeX text (usually in format r"$[text]$")
+
+            moveby (int/None) -- how many items the pointer should move by [default: 1]
+                                 (if moveby = 1: move pointer one item to right in parsed text,
+                                 if moveby = -2: move pointer two items to left in parsed text)
+                                 
+                                 Notes:
+                                       This cannot have the same type as moveto
+
+            moveto (int/None) -- move pointer to an specific place in list [default: None]
+                                 (if moveto = 2: place pointer after second item)
+                                 
+                                 Notes:
+                                       This cannot have the same type as moveby
+                                       This cannot be a negative integer.
+        
+        
+        EXAMPLE
+
+            [insert later]
+        """
+
         if moveby == moveto == None or None not in (moveby, moveto):
-            raise typemathtextError("moveby and moveto cannot be set simultaneously")
+            raise typemathtextError("'moveby' and 'moveto' must be of different types.")
 
     def concatenate_chars(self, chars, *args):
+        """Connects items in a string into one item if they concatenate to some given value(s)
+
+
+        KEYWORDS
+
+            chars (list) -- the list to be edited
+            
+            *args (str) -- list strings that need to be concatenated (in that order)
+
+
+        RETURNS
+
+            Returns a new list with the given concatenations
+
+
+        EXAMPLE
+
+            concatenate_chars(["h", "e", "l", "l", "o"], "he", "ll")
+
+            As in the list, there are two consecutive items that together create "he",
+            these are concatenated. Likewise for "ll".
+
+            This returns the list: ["he", "ll", o]
         """
-        Add this in!!
-        """
+
         output = chars
         for item in args:
             i = 0
